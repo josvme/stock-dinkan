c7525e6e06a6d3285bf6791c4d5dec165c821f5b
josv <josvme@protonmail.com>
2022-07-26 16:27:32 +0200

    Cleanup deprecated files

--- src/main/scala/filesources/DataSource.scala
+++ src/main/scala/filesources/DataSource.scala
@@ -7,17 +7,7 @@ import java.io.File
 
 object DataSource {
 
-  def getAllStockFileNames[F[_]: Monad]: F[Array[File]] = {
-
-    val fileFolder = "./stock-files/"
-    val folders: Array[File] = (new File(fileFolder)).listFiles
-      .filter(s => s.getName.endsWith("json") && s.isFile)
-      .sorted
-    folders.pure[F]
-  }
-
   def getAllStockFundamentalFileNames[F[_]: Monad]: F[Array[File]] = {
-
     val fileFolder = "./stock-fundamental-files/"
     val folders: Array[File] = (new File(fileFolder)).listFiles
       .filter(s => s.getName.endsWith("json") && s.isFile)
