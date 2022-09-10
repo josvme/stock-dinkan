c9570854e09a5d4099f89c8c037898c77f5cd3a4
josv <josvme@protonmail.com>
2022-09-10 17:26:16 +0200

    Add timeouts for sttp

--- src/main/scala/downloader/DownloaderYahoo.scala
+++ src/main/scala/downloader/DownloaderYahoo.scala
@@ -3,6 +3,8 @@ package downloader
 import cats.effect.IO
 import sttp.client3.asynchttpclient.fs2.AsyncHttpClientFs2Backend
 import sttp.client3.{UriContext, asFile, emptyRequest}
+import scala.concurrent.duration._
+import sttp.client3.SttpBackendOptions
 
 object DownloaderYahoo {
 
@@ -10,15 +12,19 @@ object DownloaderYahoo {
       ticker: String,
       config: YahooStockConfig
   ): IO[Either[String, String]] = {
-    AsyncHttpClientFs2Backend.resource[IO]().use { backend =>
-      val request = emptyRequest
-        .contentType("application/json")
-        .get(
-          uri"${RequestConfigYahoo.YAHOO_BASE_URL}/$ticker?interval=${config.timeFrame}&period1=${config.start}&period2=${config.end}&includePrePost=False&events=div%2Csplits"
-        )
+    AsyncHttpClientFs2Backend
+      .resource[IO](options = SttpBackendOptions.connectionTimeout(5.seconds))
+      .use { backend =>
+        val request = emptyRequest
+          .contentType("application/json")
+          .get(
+            uri"${RequestConfigYahoo.YAHOO_BASE_URL}/$ticker?interval=${config.timeFrame}&period1=${config.start}&period2=${config.end}&includePrePost=False&events=div%2Csplits"
+          )
 
-      val response = request.send(backend)
-      response.map(_.body)
-    }
+        val response = request
+          .readTimeout(5.seconds)
+          .send(backend)
+        response.map(_.body)
+      }
   }
 }
